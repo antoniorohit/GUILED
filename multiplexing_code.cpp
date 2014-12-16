6d89da58feac67541794780ffd9ba55cfe7b0dcb
peadark <peadar.keegan@gmail.com>
2014-12-16 07:49:52 +0000

    Update multiplexing_code.cpp
    Now works between up to 4 8x8 modules connected vertically, with up to 2 connected horizontally. Still working on that clear function

--- multiplexing_code.cpp
+++ multiplexing_code.cpp
@@ -1,21 +1,34 @@
 #include "mbed.h"
 #include "WS2811.h"
 
-#define X_MAX	64
-#define Y_MAX	64
+#define X_MAX   64
+#define Y_MAX   64
+
+#define nRows 8 //The number of rows in a basic block
+#define nCols 8 // The number of columns in a basic block
 
 // per LED: 3 * 20 mA = 60mA max
 // 60 LEDs: 60 * 60mA = 3600 mA max
 // 120 LEDs: 7200 mA max
-unsigned const nLEDs = MAX_LEDS_PER_STRIP;			// 64
+unsigned const nLEDs = MAX_LEDS_PER_STRIP;          // 64
 
+unsigned const horizontal_blocks = 2; //The number of basic blocks connected horizontally
+unsigned const vertical_blocks = 4; //The number of basic blocks connected vertically
 
+//Each mask corresponds to the nth significant bit in a binary word
 uint8_t mask_1 = 1;
 uint8_t mask_2 = 2;
 uint8_t mask_3 = 4;
-// Pins *NUMBERS* for PORTC (convenient for KL25Z freedom board)
-unsigned const DATA_OUT_PIN1 = 7; 
+uint8_t mask_4 = 8;
+uint8_t mask_5 = 16;
+uint8_t mask_6 = 32;
 
+// Pins *NUMBERS* for PORTC (convenient for KL25Z freedom board)
+
+unsigned const DATA_OUT_PIN1 = 7; // DATA OUT for the highest vertical block
+unsigned const DATA_OUT_PIN2 = 0;// The lower the PIN number, the higher it is IRL
+unsigned const DATA_OUT_PIN3 = 3;
+unsigned const DATA_OUT_PIN4 = 4;
 
 // Address Pin locations (There is some issue with the pins where only these specific pins can be used for addressing.)
 DigitalOut ADDRESS_PIN_3(D2); // AKA PTD4
@@ -24,7 +37,7 @@ DigitalOut ADDRESS_PIN_1(D5); // AKA PTA5
 
 
 
-Serial pc(USBTX, USBRX);					//  D1 (PTA2) and D0 (PTA1) on KL25Z
+Serial pc(USBTX, USBRX);                    //  D1 (PTA2) and D0 (PTA1) on KL25Z
 
 static void clearStrip(WS2811 &strip)
 {
@@ -35,53 +48,78 @@ static void clearStrip(WS2811 &strip)
     strip.show();
 }
 
+static void illuminateStrip(WS2811 &strip, uint8_t led_matrix[])
+{
+    unsigned nLEDs = strip.numPixels();
+     for (unsigned i = 0; i < nLEDs; i++) {
+        strip.setPixelColor(i, led_matrix[3*i], led_matrix[(3*i)+1], led_matrix[(3*i)+2]);
+    }
+    strip.show();
+}
+
+
+
 int main(void)
 {
-	myled = 1;
-pc.baud(115200);							// 11520 Bytes/s -> ~0.3s for 32x32 RGB pixels
-    WS2811 *lightStrip;
-    // Initialize an 8-strip matrix
-    // Note that these are all using the same Data Out Pin - NB This needs to be more scalable
-    WS2811 lightStrip1(nLEDs, DATA_OUT_PIN1);
-    WS2811 lightStrip2(nLEDs, DATA_OUT_PIN1);
-    WS2811 lightStrip3(nLEDs, DATA_OUT_PIN1);
-    WS2811 lightStrip4(nLEDs, DATA_OUT_PIN1);    
-    WS2811 lightStrip5(nLEDs, DATA_OUT_PIN1);
-    WS2811 lightStrip6(nLEDs, DATA_OUT_PIN1);
-    WS2811 lightStrip7(nLEDs, DATA_OUT_PIN1);
-    WS2811 lightStrip8(nLEDs, DATA_OUT_PIN1);
+    pc.baud(115200);                            // 11520 Bytes/s -> ~0.3s for 32x32 RGB pixels
+    
+ 
+    // Initialize one LED Strip, run from one pin. This is a placeholder for the entire block.
+
+    WS2811 lightStrip1(nLEDs,DATA_OUT_PIN1); //Highest Vertical Block (1.0)
+    WS2811 lightStrip2(nLEDs,DATA_OUT_PIN2);
+    WS2811 lightStrip3(nLEDs,DATA_OUT_PIN3); 
+    WS2811 lightStrip4(nLEDs,DATA_OUT_PIN4); // Lowest Vertical Block   (4.0)
     
     
+    /*  -------------------------------------
+        |               |                   |
+        |   BLOCK       |   BLOCK           |
+        |   1.0         |   1.1             |
+        |               |                   |
+        |_______________|___________________|
+        |               |                   |
+        |               |                   |
+        |   BLOCK       |   BLOCK           |
+        |   2.0         |   2.1             |
+        |               |                   |
+        ----------------|--------------------
+            */
     
     lightStrip1.begin();
     lightStrip2.begin();
     lightStrip3.begin();
     lightStrip4.begin();
-    lightStrip5.begin();
-    lightStrip6.begin();
-    lightStrip7.begin();
-    lightStrip8.begin();
-
-    uint8_t q = 0;								// index variable
-    uint8_t char_buff[10];							// UART buffer
-    uint8_t curr_char = 0;							// current byte read from UART
-    uint8_t	j;									// for - loop iteration variable
+    
+    uint8_t q = 0;                              // index variable
+    uint8_t char_buff[10];                          // UART buffer
+    uint8_t curr_char = 0;                          // current byte read from UART
+   
+  
+    uint8_t led_matrix[nRows*vertical_blocks][3*nCols*horizontal_blocks] = {0};           // Define a matrix to store RGB info about each LED in the array
+    
+    uint8_t ledmatrix_row1[3*nCols*horizontal_blocks] = {0};        // Temporary variables to take in each row in an array for input to a function
+    uint8_t ledmatrix_row2[3*nCols*horizontal_blocks] = {0};		// Each array represents a separate data pin. All data pins are driven simultaeneously	
+    uint8_t ledmatrix_row3[3*nCols*horizontal_blocks] = {0};		// to keep the matrix correct.
+    uint8_t ledmatrix_row4[3*nCols*horizontal_blocks] = {0};
+    
+  
     
     for (;;) {
+        
         /* I have defined a custom UART packet protocol
          * A general packet may look like: 
-         * char_buff[0]		start byte: 	254
+         * char_buff[0]     start byte:     254
          * char_buff[1]     x-coordinate [0, 63]
          * char_buff[2]     y-coordinate [0, 63]
-         * char_buff[3]     R (6-bit)	 [0, 63]
-         * char_buff[4]     G (6-bit)	 [0, 63]
-         * char_buff[5]     B (6-bit)	 [0, 63]
-         * char_buff[6]     Delimiter: 		255
+         * char_buff[3]     R (6-bit)    [0, 63]
+         * char_buff[4]     G (6-bit)    [0, 63]
+         * char_buff[5]     B (6-bit)    [0, 63]
+         * char_buff[6]     Delimiter:      255
          */
         while(curr_char != 255 && q < 7)
         {
             /* If there is UART traffic */
-          //  while( ! pc.readable());
             if(pc.readable())
             {
                 curr_char = uint8_t(pc.getc());
@@ -90,104 +128,79 @@ pc.baud(115200);							// 11520 Bytes/s -> ~0.3s for 32x32 RGB pixels
             }
         }
         
+        
+         
+        //Store the RGB values at the correct place in the array
+        led_matrix[char_buff[2]][3*(char_buff[1]%X_MAX)]   = char_buff[3];
+        led_matrix[char_buff[2]][3*(char_buff[1]%X_MAX)+1] = char_buff[4];
+        led_matrix[char_buff[2]][3*(char_buff[1]%X_MAX)+2] = char_buff[5]; 
+   
+   
+   ////////////////////////// Writing the colours to the LED's //////////////////////////////////////
+   
+        
+        unsigned vert_pos = char_buff[2]%Y_MAX; // The absolute vertical position in the structure
+        
+        unsigned row_pos = char_buff[2] % 8 ; // The relative position of the strip being actuated
+        
         if((char_buff[0] == 254) && (q >= 5)){
             
-               // This also needs to be more scalable. Considering using for - loop
-            switch(char_buff[2]%Y_MAX){
-            case 0:
-
-                ADDRESS_PIN_1 =  0; //Change the value on each address pin
-                ADDRESS_PIN_2 =  0;
-                ADDRESS_PIN_3 =  0;
-                lightStrip = &lightStrip1;
-                myled = 0;
-                break;
-            case 1:
-
-                ADDRESS_PIN_1 =  1; //Change the value on each address pin
-                ADDRESS_PIN_2 =  0;
-                ADDRESS_PIN_3 =  0;
-                lightStrip = &lightStrip2;
-                myled = 0;
-                break;
-            case 2:
-                ADDRESS_PIN_1 =  0; //Change the value on each address pin
-                ADDRESS_PIN_2 =  1;
-                ADDRESS_PIN_3 =  0;
-                lightStrip = &lightStrip3;
-                myled =0;
-                break;
-            case 3:
-                ADDRESS_PIN_1 =  1; //Change the value on each address pin
-                ADDRESS_PIN_2 =  1;
-                ADDRESS_PIN_3 =  0;
-                lightStrip = &lightStrip4;
-                myled = 0;
-                break;
-            case 4:
-                ADDRESS_PIN_1 =  0; //Change the value on each address pin
-                ADDRESS_PIN_2 =  0;
-                ADDRESS_PIN_3 =  1;
-                lightStrip = &lightStrip5;
-                myled = 0;
-                break;
-            case 5:
-                ADDRESS_PIN_1 =  1; //Change the value on each address pin
-                ADDRESS_PIN_2 =  0;
-                ADDRESS_PIN_3 =  1;
-                lightStrip = &lightStrip6;
-                break;
-            case 6:
-                ADDRESS_PIN_1 =  0; //Change the value on each address pin
-                ADDRESS_PIN_2 =  1;
-                ADDRESS_PIN_3 =  1;
-                lightStrip = &lightStrip7;
-                break;
-            case 7:
-                ADDRESS_PIN_1 =  1; //Change the value on each address pin
-                ADDRESS_PIN_2 =  1;
-                ADDRESS_PIN_3 =  1;
-                lightStrip = &lightStrip8;
-                break;        
-            default:
-                ADDRESS_PIN_1 =  1; //Change the value on each address pin
-                ADDRESS_PIN_2 =  1;
-                ADDRESS_PIN_3 =  1;
-                lightStrip = &lightStrip8;
-                q = 0;
-                break;   
-            }
             
-           
-            
-            //Antonio's Original Code
+                ADDRESS_PIN_1 =  mask_1 & vert_pos; //Change the value on each address pin
+                ADDRESS_PIN_2 =  mask_2 & vert_pos;
+                ADDRESS_PIN_3 =  mask_3 & vert_pos;
+             
+               
+      
+                
+                    for (int col = 0; col < 3*nCols*horizontal_blocks; col++){
+                    // This is the data to be sent out on each data pin
+                            ledmatrix_row1[col] = led_matrix[row_pos] [col];
+                            if (vertical_blocks > 1) {
+                            	ledmatrix_row2[col] = led_matrix[row_pos+nRows] [col];}	
+                            if (vertical_blocks > 2) {
+                            	ledmatrix_row3[col] = led_matrix[row_pos+2*nRows] [col];}
+                            if (vertical_blocks > 3) {
+                            	ledmatrix_row4[col] = led_matrix[row_pos+3*nRows] [col];}
+                         }
+                         
+               
+               //		Send the data out into the real world...
+                        illuminateStrip(lightStrip1, ledmatrix_row1);
+                        
+                        if (vertical_blocks > 1) {
+                       		illuminateStrip(lightStrip2, ledmatrix_row2);}
+                        if (vertical_blocks > 2) {
+                        	illuminateStrip(lightStrip3, ledmatrix_row3);}
+                        if (vertical_blocks > 3) {
+                        	illuminateStrip(lightStrip4, ledmatrix_row4);}
    
-            // 								{PIXEL_POS (X), 		R, 				G, 				B}
-            (*lightStrip).setPixelColor(char_buff[1]%X_MAX, (char_buff[3]), (char_buff[4]), (char_buff[5]));
-         
-            (*lightStrip).show();
-            
-        	
+           
+                
             pc.putc(48+(char_buff[1]));
             
         }
         else{   
-        		// This should scroll through each strip and clear them, but it doesn't.
-        		for (j = 0; j < 8 ; j ++)      {  
-        		
-           			clearStrip(lightStrip1);
-           			clearStrip(lightStrip2);
-            		clearStrip(lightStrip3);
-		            clearStrip(lightStrip4);
-        		    clearStrip(lightStrip5);
-     		    	clearStrip(lightStrip6);
-           	 		clearStrip(lightStrip7);
-            		clearStrip(lightStrip8);
-            		
-            		ADDRESS_PIN_1 = j & mask_1;
-            		ADDRESS_PIN_2 = j & mask_2;
-            		ADDRESS_PIN_3 = j & mask_3;
-            		}
+                
+                // Clear function. Still a little tempermental.
+                  for (int col = 0; col < 3*nCols*horizontal_blocks; col++){
+                    for (int row = 0; row < nRows*vertical_blocks; row ++) {
+                      
+                       led_matrix[row][col] = 0; //Set the entire matrix to zero
+                        } 
+                    }
+                    
+                    for (int j = 0; j <nRows; j ++) {
+                    	
+                    	 ADDRESS_PIN_1 =  mask_1 & j; 
+                		 ADDRESS_PIN_2 =  mask_2 & j;
+                		 ADDRESS_PIN_3 =  mask_3 & j;
+            	
+            			clearStrip(lightStrip1);
+                		clearStrip(lightStrip2);
+                		clearStrip(lightStrip3);
+                		clearStrip(lightStrip4);
+                		}                  
         }
         q = 0;
         curr_char = 0;
